de185cb178a63b45f27e78528aa34effc32e544a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-29 13:12:06 +0100

    Added clippy lint set.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,29 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! Library core.
+
+#![warn(
+    clippy::all,
+    clippy::cargo,
+    clippy::missing_docs_in_private_items,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::restriction
+)]
+#![allow(
+    clippy::as_conversions,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::else_if_without_else,
+    clippy::float_arithmetic,
+    clippy::implicit_return,
+    clippy::indexing_slicing,
+    clippy::integer_arithmetic,
+    clippy::integer_division,
+    clippy::modulo_arithmetic,
+    clippy::multiple_inherent_impl,
+    clippy::panic,
+    clippy::print_stdout,
+    clippy::unreachable,
+    clippy::unwrap_used
+)]
