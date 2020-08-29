c411c3bd313e436fd186c4c4a99e6270b960951c
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-08-29 16:49:18 +0100

    Added drawing to site.

---
+++ web/drawing.js
@@ -0,0 +1,54 @@
+/// Drawn cell size.
+export const CELL_SIZE = 10; // [px]
+/// Grid colour.
+const GRID_COL = "#CCCCCC";
+/// Dead cell colour.
+const DEAD_COL = "#FFFFFF";
+/// Living cell colour.
+const ALIVE_COL = "#000000";
+
+
+
+/// Draw the grid array.
+export function draw_grid(ctx, width, height) {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COL;
+
+    for (let j = 0; j <= height; ++j) {
+        ctx.moveTo(0, (j * (CELL_SIZE + 1)) + 1);
+        ctx.lineTo(((CELL_SIZE + 1) * width) + 1, (j * (CELL_SIZE + 1)) + 1);
+    }
+    for (let i = 0; i <= width; ++i) {
+        ctx.moveTo((i * (CELL_SIZE + 1)) + 1, 0);
+        ctx.lineTo((i * (CELL_SIZE + 1)) + 1, ((CELL_SIZE + 1) * height) + 1);
+    }
+
+    ctx.stroke();
+}
+
+/// Draw the cell array.
+export function draw_cells(ctx, width, height, board) {
+    const cells_ptr = board.cells_ptr();
+    const cells = new Uint8Array(memory.buffer, cells_ptr, (width * height));
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; ++row) {
+        for (let col = 0; col < width; ++col) {
+            const idx = get_index(row, col);
+
+            ctx.fillStyle = cells[idx] == 0 ?
+                DEAD_COL :
+                ALIVE_COL;
+
+            ctx.fillRect(
+                (col * (CELL_SIZE + 1)) + 1,
+                (row * (CELL_SIZE + 1)) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    ctx.stroke();
+}
--- web/index.js
+++ web/index.js
@@ -5,9 +5,32 @@ import {
 import {
     memory
 } from "museum/museum_bg";
+import {
+    draw_cells,
+    draw_grid,
+    CELL_SIZE
+} from "./drawing";
 
 
 
-/// Main board.
-// const board = Board.new(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-const board = Board.new(64, 64);
+/// Canvas id.
+const canvas = document.getElementById("main_canvas");
+/// Drawing context.
+const ctx = canvas.getContext('2d');
+
+
+
+/// Draw the grid array.
+function setup_new_grid(width, height) {
+    canvas.height = (CELL_SIZE + 1) * height + 1;
+    canvas.width = (CELL_SIZE + 1) * width + 1;
+
+    /// Main board.
+    const board = Board.new(width, height);
+    board.randomise(0.5);
+
+    draw_grid(ctx, width, height);
+    draw_cells(ctx, width, height, board);
+}
+
+setup_new_grid(64, 64);
